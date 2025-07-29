a801cd8decfc6cccf43bdf401526945cd6bd6ece
Brayan <144967324+BrayanOmar0508@users.noreply.github.com>
2025-07-29 11:22:25 -0400

    Update likeCommands.py

--- cogs/likeCommands.py
+++ cogs/likeCommands.py
@@ -166,7 +166,7 @@ class LikeCommands(commands.Cog):
 
                 data = await response.json()
                 embed = discord.Embed(
-                    title="LIKES PARA FREE FIRE",
+                    title="``LIKES PARA FREE FIRE``",
                     color=0x2ECC71 if data.get("status") == 1 else 0xE74C3C,
                     timestamp=datetime.now()
                 )
